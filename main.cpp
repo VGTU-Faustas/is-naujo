72c865fa5711c4a6673ecc3bda7fea485b334ae3
VGTU-Faustas <48588311+VGTU-Faustas@users.noreply.github.com>
2019-04-05 14:32:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,29 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
+#include <vector>
 
 using namespace std;
+struct my{
+string var, pav;
+vector <int> paz;
+};
 
-struct mokinys
-{
-    string vardas, pavarde;
-    int egz;
-    double galutinis;
-
-}ID[100];
-
-int main()
-{
-    int i=1, j=0, k=0, a=0,b=0, m=0;
-    while( i==1)
-    {
-        char tn;
-        cout << "Ar norite ivesti nauja mokini i sistema? (t/n): " << endl;
-        cin >> tn;
-        if (tn == 'n')
-        {
-            i=0;
-        }
-        else if (tn =='t')
-        {
-            i=1;
-            cout << "Mokinio vardas: " << endl;
-            cin >> ID[j++].vardas;
-                cout << "Mokinio pavarde: "<< endl;
-                cin >> ID[k++].pavarde;
-                cout << "Egzamino balas: ";
-                cin >> ID[a++].egz;
-                int ND[100], tarpinis=0;
-                for (int h=0; h<99; h++)
-                {
-                    cout << "iveskite namu darbu pazymius (0 nutraukia ivedima): ";
-                    cin >> ND[h];
-                    if (ND[h]==0){
-                        break;
-                        ID[b++].galutinis=(tarpinis+ID[m++].egz)/h;
-                    }
-                    tarpinis+ND[h];
-
-                }
-
-        }
-    }
-    return 0;
+int main() {
+  vector <my> lent;
+  my temp;
+  int int_temp=1;
+  cout << "Vardas <tarpas> pavarde: ";
+  cin>>temp.var>>temp.pav;
+  cout<<"iveskite pazymius";
+  while (int_temp!=0){
+    cin>>int_temp;
+    if(int_temp>=1 && int_temp<=10) temp.paz.push_back(int_temp);
+    else cout<<"Klaidinga ivestis"<<endl;
+  
+  }
+  lent.push_back(temp);
+  cout<<lent[0].var<<endl;
+  cout<<lent[0].pav<<endl;
+  for (int a: lent[0].paz)
+  cout<<a<<"; ";
 }
