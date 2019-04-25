5ce0a7caf4cac11c9993b8e1dc1db5ee2c78057f
VGTU-Faustas <48588311+VGTU-Faustas@users.noreply.github.com>
2019-04-25 23:26:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,43 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-struct mokinys
-{
-    string vardas, pavarde;
-    int egz;
-    double galutinis;
-
-}ID[100];
+struct my {
+    string var, pav;
+    vector <int> paz; // Namu darbai
+    int kd; // kontrolis darbas
+    double vid; //vidurkis
+};
 
 int main()
 {
-    int i=1, j=0, k=0, a=0,b=0, m=0;
-    while( i==1)
-    {
-        char tn;
-        cout << "Ar norite ivesti nauja mokini i sistema? (t/n): " << endl;
-        cin >> tn;
-        if (tn == 'n')
-        {
-            i=0;
+    vector <my> lent;
+    my temp; int int_temp=1; char uzkl; my vid;
+    do{
+        int_temp=1;
+
+        cout<<"Iveskite: Vardas <tarpas> Pavarde: "; cin>>temp.var>>temp.pav;
+        cout<<"Iveskite pazymius: "; temp.paz.clear();
+        while(int_temp!=0){
+            cin>>int_temp;
+            if(int_temp>=1 && int_temp<=10) temp.paz.push_back(int_temp);
+                else cout<<"Ivestis klaidinga arba nutraukta " << endl;
+
         }
-        else if (tn =='t')
-        {
-            i=1;
-            cout << "Mokinio vardas: " << endl;
-            cin >> ID[j++].vardas;
-                cout << "Mokinio pavarde: "<< endl;
-                cin >> ID[k++].pavarde;
-                cout << "Egzamino balas: ";
-                cin >> ID[a++].egz;
-                int ND[100], tarpinis=0;
-                for (int h=0; h<99; h++)
-                {
-                    cout << "iveskite namu darbu pazymius (0 nutraukia ivedima): ";
-                    cin >> ND[h];
-                    if (ND[h]==0){
-                        break;
-                        ID[b++].galutinis=(tarpinis+ID[m++].egz)/h;
-                    }
-                    tarpinis+ND[h];
+        lent.push_back(temp);
+        cout<<"Iveskite kontrolinio darbo rezultata: "; cin>>temp.kd;
+        cout<< "Ivesti dar viena? (t/n) "; cin>>uzkl;
+        }
+        while (uzkl!='n');
 
-                }
-
+        for (my z: lent){
+            cout<<z.var<<endl;
+            cout<<z.pav<<endl;
+            for (int a: z.paz)
+            cout<<a<<"; ";
+            cout<<endl;
         }
     }
-    return 0;
-}
