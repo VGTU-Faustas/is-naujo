eee200e7c54ab898540508ae50788b4470fb6d8d
VGTU-Faustas <48588311+VGTU-Faustas@users.noreply.github.com>
2019-05-17 10:31:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,76 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include <vector>
+#include <iterator>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
-struct my{
-string var, pav;
-vector <int> paz;
+
+struct my {
+    string var, pav;
+    vector <int> paz; // Namu darbai
+    int kd; // kontrolis darbas
+    double vid; //vidurkis
 };
 
-int main() {
-  vector <my> lent;
-  my temp;
-  int int_temp=1;
-  cout << "Vardas <tarpas> pavarde: ";
-  cin>>temp.var>>temp.pav;
-  cout<<"iveskite pazymius";
-  while (int_temp!=0){
-    cin>>int_temp;
-    if(int_temp>=1 && int_temp<=10) temp.paz.push_back(int_temp);
-    else cout<<"Klaidinga ivestis"<<endl;
-  
-  }
-  lent.push_back(temp);
-  cout<<lent[0].var<<endl;
-  cout<<lent[0].pav<<endl;
-  for (int a: lent[0].paz)
-  cout<<a<<"; ";
-}
+double vidurkis( vector <int> paz);
+double mediana(vector <int> paz);
+
+int main()
+{
+    vector <my> lent;
+    my temp; int int_temp=1; char uzkl, uzkl2; my vid;
+    do{
+        int_temp=1;
+
+        cout<<"Iveskite: Vardas <tarpas> Pavarde: "; cin>>temp.var>>temp.pav;
+        cout<<"Iveskite pazymius: "; temp.paz.clear();
+        while(int_temp!=0){
+            cin>>int_temp;
+            if(int_temp>=1 && int_temp<=10) temp.paz.push_back(int_temp);
+                else cout<<"Ivestis klaidinga arba nutraukta " << endl;
+
+        }
+        lent.push_back(temp);
+        cout<<"Iveskite kontrolinio darbo rezultata: "; cin>>temp.kd;
+        cout<< "Ivesti dar viena? (t/n) "; cin>>uzkl; cout << endl;
+        }
+        while (uzkl!='n');
+
+        cout<<"Vidurkis/mediana (v/m)?"; cin >>uzkl2;
+        if (uzkl2=='m') double v=mediana(temp.paz);
+                double v=vidurkis(temp.paz);
+
+        cout << setfill ('_') << setw (60) << '\n' << setfill(' ');
+        cout.width(20);
+        cout<< left <<"VARDAS";cout.width(20);
+        cout<< left << "PAVARDE";cout.width(19);
+        cout<< right << "VIDURKIS" << '\n';
+
+        for (my z: lent){
+            cout.width(20);
+            cout<<z.var;
+            cout.width(20);
+            cout<<z.pav;
+            cout.width(20);
+         //   for (int a: z.paz)
+         //   cout<<a<<"; "<<endl;
+           // cout<<endl;
+            cout<<v<<'\n';
+        }
+    }
+
+    double vidurkis( vector <int> paz)
+    {
+
+         return accumulate(paz.begin(), paz.end(), 0.0)/paz.size() ;
+
+    }
+
+    double mediana(vector <int> paz)
+    {
+        sort(paz.begin(), paz.end());
+        return (paz.size()%2==0) ? (paz[paz.size()/2-1]+paz[paz.size()/2])/2 : paz[paz.size()/2];
+    }
